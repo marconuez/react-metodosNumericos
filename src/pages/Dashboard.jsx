db7cce34dd5ded8a10edd98cb749c8e8538dd985
Rafael Molina <107267660+Hiden35@users.noreply.github.com>
2022-11-06 14:35:14 -0600

    tomela
    tomela

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -24,7 +24,7 @@ export const Dashboard = () => {
                 <div>
                     <div className='btn btn-dark pointer'>
                         <h3>Marco Nuñez Cabezas</h3>
-                        <h4>Front-End, Scrum Master HD 4K, PRO-fornites, Tifón, Maquina, #1, Sucesor de York </h4>
+                        <h4>Front-End</h4>
                     </div>
                 </div>
             </div>
